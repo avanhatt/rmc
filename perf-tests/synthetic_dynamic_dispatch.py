bc9a2f37e3505c42fb97f26d7073b8b6b03764b1
Alexa VanHattum <alexavanhattum@gmail.com>
2021-09-08 16:03:03 +0000

    more script

--- perf-tests/synthetic_dynamic_dispatch.py
+++ perf-tests/synthetic_dynamic_dispatch.py
@@ -30,8 +30,10 @@ main_def = """
 fn main() {{
     for i in 0..{iterations} {{
         let s : &dyn A = if i % {functions} == 0 {{
-            Struct0 {{}} as &dyn A
+            &Struct0 {{}} as &dyn A
         {body}
+        }} else {{
+            unreachable!()
         }};
         assert!(s.f(i) == 1); 
     }}
@@ -40,7 +42,7 @@ fn main() {{
 
 loop_body_case_def = """
         }} else if i % {functions} == {index} {{
-            Struct{index} {{}} as &dyn A
+            &Struct{index} {{}} as &dyn A
 """
 
 smack_prelude = """
@@ -56,7 +58,10 @@ def build_rs(iterations, functions):
     # Define a struct per dynamic function
     for i in range(functions):
         # Failure cases at the end of the iterations
-        rs += struct_def.format(index = i, case = iterations - i)
+        case = iterations - i
+        if i == (iterations - 1) % functions:
+            case = iterations - 1
+        rs += struct_def.format(index = i, case = case)
 
     # Build the "if else" cases of the loop body
     body = ""
@@ -71,6 +76,7 @@ def build_rs(iterations, functions):
 
 def main():
 
+    # Parse arguments
     parser = argparse.ArgumentParser(description='Run and time synthetic dynamic dispatch tests')
     parser.add_argument('--funs', type=int, help='Number of dynamic functions (minimum if maximum is provided)', default=2)
     parser.add_argument('--maxfuns', type=int, help='Maximum number of dynamic functions')
@@ -81,11 +87,16 @@ def main():
     # Default to single parameters if no maximum provided
     maxfuns = args.maxfuns if args.maxfuns else args.funs
     maxi = args.maxi if args.maxi else args.i
-
     assert(maxfuns >= args.funs)
     assert(maxi >= args.i)
 
-    rs = build_rs(args.i, args.funs)
+    for i in range(args.i, maxi + 1):
+        for f in range(args.funs, maxfuns + 1):
+            rs = build_rs(i, f)
+            filename = "dynamic_dispatch_{}i_{}f.rs".format(i, f)
+            print(filename)
+            with open(filename, 'w') as writer:
+                writer.write(rs)
 
 
 if __name__ == "__main__":
